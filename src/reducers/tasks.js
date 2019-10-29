aef99d86d341e61d4c4af7f0251c780d399dd341
dan-kuzbass <guzikov1996@mail.ru>
2019-10-29 09:29:06 +0700

    edit init reducer

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,13 +1,7 @@
 import * as types from "../constants/typesReducer";
 
 const initialState = {
-  tasks: [
-    { id: "3", name: "Поспать", date: "01.01.2014" },
-    { id: "4", name: "Поработать", date: "01.01.2015" },
-    { id: "5", name: "Сходить в магаз", date: "01.01.2016" },
-    { id: "1", name: "Сварить кофе", date: "01.01.2012" },
-    { id: "2", name: "Пообедать", date: "01.01.2013" }
-  ]
+  tasks: []
 }
 
 export function tasks(state = initialState, action) {
